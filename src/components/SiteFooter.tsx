9c7a5bea7f4b49808934f2449d8803ccb894c141
xCaptaiN09 <dilsheddilu123@gmail.com>
2026-09-11 23:17:18 +0530

    style: remove superscript marks from footer socials

--- src/components/SiteFooter.tsx
+++ src/components/SiteFooter.tsx
@@ -4,18 +4,9 @@ export function SiteFooter() {
   return (
     <footer className="site-footer">
       <div className="footer-socials">
-        <a href="https://github.com/xCaptaiN09" target="_blank" rel="noreferrer">
-          {'{GitHub'}
-          <span className="nav-sup">*</span>
-        </a>
-        <a href="https://linkedin.com/in/muhammed-dilshad-a-809311326" target="_blank" rel="noreferrer">
-          {'{LinkedIn'}
-          <span className="nav-sup">//</span>
-        </a>
-        <a href="mailto:hello.dilshad.in@gmail.com">
-          {'{Mail'}
-          <span className="nav-sup">+</span>
-        </a>
+        <a href="https://github.com/xCaptaiN09" target="_blank" rel="noreferrer">GitHub</a>
+        <a href="https://linkedin.com/in/muhammed-dilshad-a-809311326" target="_blank" rel="noreferrer">LinkedIn</a>
+        <a href="mailto:hello.dilshad.in@gmail.com">Mail</a>
       </div>
 
       <div className="footer-cols">
